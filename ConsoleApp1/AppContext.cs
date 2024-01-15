ecbd3b2756ffa6d2ce128ffccdfabed6aa2b0d72
N1K1111 <nik.glazkov2016@yandex.ru>
2024-01-15 20:31:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        /*
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+        */
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=pgdb; POSTGRES_USER=postgres; POSTGRES_PASSWORD=123123;");
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AppContext appContext = new AppContext())
+            {
+                User user = new User { FirstName = "ник", LastName = "маккен", Age = 20 };
+                appContext.Users.Add(user);
+                appContext.SaveChanges();
+            }
+
+            using (AppContext appContext = new AppContext())
+            {
+                var user = appContext.Users.ToList();
+                
+                foreach(User us in user)
+                {
+                    Console.WriteLine($"{us.LastName} {us.FirstName} {us.Age}");
+                }
+            }
+
+            
+
+        }
+
+
+    }
+}
